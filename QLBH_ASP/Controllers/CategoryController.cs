83ab674f6f83ce219dde9f927851070d92e4394c
NguyenMinhTri2394 <ngminhtri.dev@gmail.com>
2024-12-11 14:43:09 +0700

    First update

---
+++ QLBH_ASP/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBH_ASP.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult AllCategory()
+        {
+            return View();
+        }
+
+        // GET: Category
+        public ActionResult ProductByCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ QLBH_ASP/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLBH_ASP.Context;
+
+namespace QLBH_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        QLBH_ASPEntities objQLBH_ASPEntities = new QLBH_ASPEntities();
+
+        public ActionResult Index()
+        {
+            var lstProduct = objQLBH_ASPEntities.Products.ToList();
+            return View(lstProduct);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
